e06af029fdb2dbcca2879372a486d7a48564aa75
PrajwalPawar789 <prajwalrpawar2001@gmail.com>
2026-02-08 03:33:19 +0530

    new change's

---
+++ index.js
@@ -0,0 +1,21 @@
+import { ImapFlow } from "imapflow";
+
+const client = new ImapFlow({
+  host: process.env.IMAP_HOST,
+  port: 993,
+  secure: true,
+  auth: { user: process.env.IMAP_USER, pass: process.env.IMAP_PASS }
+});
+
+(async () => {
+  await client.connect();
+  console.log("Connected");
+  const boxes = await client.list();
+  console.log("Mailboxes:", boxes.map(b => b.path));
+  await client.logout();
+  console.log("Logged out");
+  process.exit(0);
+})().catch(err => {
+  console.error("IMAP failed:", err);
+  process.exit(1);
+});
